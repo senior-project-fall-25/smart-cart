293aa0bf258ce2e2225083749f7fb3f097106f3a
maryhanson11 <mary.hanson.dev@gmail.com>
2025-10-24 18:25:45 -0400

    Finished profile creation initial userf low, added custom popup and search components to reuse

--- smart-cart/app/Profile-Creation/set-allergens.tsx
+++ smart-cart/app/Profile-Creation/set-allergens.tsx
@@ -1,14 +1,43 @@
-import { Text, View,Image, Button, StyleSheet, TouchableOpacity, ScrollView} from "react-native";
+import { Text, View,Image, Button, StyleSheet, TouchableOpacity, ScrollView, Modal} from "react-native";
 import { Link, useRouter } from "expo-router";
 import { HeaderTitle } from "@react-navigation/elements";
 import { useState } from "react";
-
+import PopUp from '../../components/popUp'
+import Search from "../../components/SearchBar";
+import allergenNames from "../../api/allergensNames"
+import AddAllergens from "./add-allergens";
 
 
 export default function SetAllergens() {
     const router = useRouter();
     const [allergens, setAllergens] = useState(['Peanuts', 'Gluten', 'Dairy', 'Pineapple', 'Mango'])
     const [tagged, setTagged] = useState<string[]>([]);
+    const [modalVisable, setModalVisable] = useState(false);
+    const [addedAllergens,setAddedAllergens] = useState<Array<string>>([]);
+
+    // transform allergens data into string array:
+
+
+    function closePopUp() {
+        setModalVisable(false);
+    }
+
+    function acceptPopUp () {
+
+        var newTags = [...tagged, ...addedAllergens.filter((item)=> !tagged.includes(item))];
+        
+        setTagged(newTags)
+
+        // make the allergens only tagged after add allergens??
+
+        var newAllergens = [...allergens, ...addedAllergens.filter((item) => !allergens.includes(item))];
+        setAllergens(newTags);
+        setModalVisable(false);
+
+        // clear added allergens
+        setAddedAllergens([]);
+    }
+
 
     function selectAllergen(allergen: string){
         var newTags: string[] = [...tagged]
@@ -49,9 +78,14 @@ export default function SetAllergens() {
 
                     ))
                     }
-               
+                    <View style={{width: '100%'}}>
+                        <Button onPress={() => {setModalVisable(true)}} title="Add Allergen +"/>
+                    </View>
                 
             </ScrollView>
+            
+
+            <PopUp modalVisable={modalVisable} acceptPopUp={acceptPopUp} closePopUp={closePopUp} buttonText={"Add Allergens"} Content={() => <AddAllergens selected={addedAllergens} setSelected={setAddedAllergens}/>}/>
 
             <TouchableOpacity
                 onPress={()=> router.push({pathname: './finish-profile', params: {'tagged' : JSON.stringify(tagged)}})}
@@ -70,6 +104,20 @@ export default function SetAllergens() {
 }
 
 const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        borderRadius: 5,
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+        alignContent: 'center',
+        height: '50%',
+        width: '80%',
+    },
     container: {
         flex: 1,
         alignItems: "center",
@@ -102,7 +150,7 @@ const styles = StyleSheet.create({
     },
     scrollBox: {
         height: '40%',
-        width: '80%',
+        width: '90%',
         padding: 10,
         flexDirection: 'row',
         flexWrap: 'wrap'
---
+++ smart-cart/components/SearchBar.tsx
@@ -0,0 +1,128 @@
+import React,{ useEffect, useState } from "react";
+import { View, TextInput, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import { SearchBar } from "react-native-screens";
+
+
+interface Props {
+    data: Array<string>;
+    setSelected: (selected : string) => void; 
+    selected: string;
+}
+
+const Search = ({data, setSelected, selected}: Props) => {
+    // data used to filter autocomplete results an array of string values ex: ['Milk','Peanuts',...]
+    // setSelected, selected are use state variables that store the chosen autocomplete value from dropdown
+
+    // so when you chose the value from the dropdown that will show in the text input (if you dont flush it in your parent component)
+    const [text, setText] = useState<string>(selected);
+    const [filteredData, setFilteredData] = useState<Array<string>>([]);
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    console.log(selected)
+
+    const maxItems = 5;
+    
+
+    const handleChange = (text: string) => {
+
+        setText(text)
+
+        if(text.trim()){
+
+            var filtered = [];
+
+            const currText = text.toLowerCase().trim();
+
+            console.log(currText);
+            
+            filtered = data.filter(function(item) {
+                return item.toLowerCase().startsWith(currText)
+            });
+
+            console.log(filtered);
+
+            if (filtered.length == 0){ // if no results
+                filtered.push("No results! Try searching for a synonym of your allergy.")
+            } 
+
+            setFilteredData(filtered);
+
+            setShowDropdown(true);
+
+        } else {
+            setFilteredData([""]);
+            setShowDropdown(false);
+        }
+    };
+
+
+    function handleSelect (item: string) {
+        setSelected(item);
+        
+    };
+
+
+    return (
+        <View style={styles.container}>
+        <TextInput 
+            style={[styles.searchBox, styles.body]} 
+            value={text}
+            onChangeText={handleChange}
+            placeholder="Enter item..."
+        />
+        {showDropdown && (
+            <FlatList
+            style={styles.dropdown}
+            data={filteredData}
+            keyExtractor={(item, index) => item}
+            renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => handleSelect(item)} style={styles.item}>
+                    <Text style={styles.body}>{item}</Text>
+                </TouchableOpacity>
+            )}
+            />
+        )}
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    searchBox: {
+        width: '80%', 
+        borderColor: 'lightgray',
+        borderWidth: 0.5,
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: 'white'
+    },
+    dropdown: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        marginTop: 4,
+        borderRadius: 8,
+        height: 100,
+        width: '80%',
+        backgroundColor: 'white',
+        position: 'absolute',
+        top: 45, 
+        zIndex: 1000, 
+        elevation: 10,
+    },
+    item: {
+        padding: 12,
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee',
+    },
+    body:{
+        fontSize: 16,
+        fontFamily:'Monserrat',
+        color: '#303030',
+    },
+    container: {
+        width: '100%',
+        alignItems: 'center',
+    },
+})
+
+export default Search
