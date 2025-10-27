bf389d07c994be90eb3b6cd0cc0c06e38b15e9f0
mglaum <maddieglaum@gmail.com>
2025-10-27 13:23:06 -0400

    product details ui update

--- smart-cart/app/SmartCartStyles.tsx
+++ smart-cart/app/SmartCartStyles.tsx
@@ -3,20 +3,26 @@ import { Text } from "react-native";
 import React from "react";
 
 const ProductText = ({ children }: { children: React.ReactNode }) => (
-  <Text style={{ fontFamily: "DM-Sans", textAlign: "left", textTransform: "capitalize" }}>
-    {children}
-  </Text>
+    <Text style={{ fontFamily: "DM-Sans", textAlign: "left", textTransform: "capitalize" }}>
+        {children}
+    </Text>
 );
 
 const ProductHeader = ({ children }: { children: React.ReactNode }) => (
-  <Text style={{ fontFamily: "DM-Sans-Bold", textAlign: "left", textTransform: "capitalize" }}>
-    {children}
-  </Text>
+    <Text style={{ fontFamily: "DM-Sans-Medium", fontWeight: "800", textAlign: "left", textTransform: "capitalize", flexWrap: "wrap" }}>
+        {children}
+    </Text>
 );
 
-export { ProductText, ProductHeader };
+const ProductBrand = ({ children }: { children: React.ReactNode }) => (
+    <Text style={{ fontFamily: "DM-Sans", fontWeight: "600", textAlign: "left", textTransform: "capitalize", color: "gray", fontSize: 12 }}>
+        {children}
+    </Text>
+);
+
+export { ProductText, ProductHeader, ProductBrand };
 
 // (optional, to keep Expo Router happy if it’s still inside /app)
 export default function Empty() {
-  return null;
+    return null;
 }
